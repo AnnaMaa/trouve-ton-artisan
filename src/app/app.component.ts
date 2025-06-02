32f35c76a85e95754580dee6c1cda13faa3ae331
Anais Maaroufi <anaismaaroufi@gmail.com>
2025-06-02 19:36:51 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CookieBannerComponent } from "./shared/cookie-banner/cookie-banner.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent, CookieBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'trouve-ton-artisan';
+  title = 'Trouve ton artisan';
 }
