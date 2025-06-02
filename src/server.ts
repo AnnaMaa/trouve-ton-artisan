03ec489384f4e2078556942419920dc25d7f2d71
Anais Maaroufi <anaismaaroufi@gmail.com>
2025-06-02 21:42:20 +0200

    ajout fichier dist

--- src/server.ts
+++ src/server.ts
@@ -65,28 +65,5 @@ if (isMainModule(import.meta.url)) {
  */
 export const reqHandler = createNodeRequestHandler(app);
 
-export function getPrerenderParams() {
-  return {
-    '/artisan/:id': [
-      { id: '1' },
-      { id: '2' },
-      { id: '3' },
-      { id: '4' },
-      { id: '5' },
-      { id: '6' },
-      { id: '7' },
-      { id: '8' },
-      { id: '9' },
-      { id: '10' },
-      { id: '11' },
-      { id: '12' },
-      { id: '13' },
-      { id: '14' },
-      { id: '15' },
-      { id: '16' },
-      { id: '17' },
-      
-    ]
-  };
-}
+
 
